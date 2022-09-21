b62bec1aa73973673ca981dfe9b95e0b630e8866
kawenav <111808380+kawenav@users.noreply.github.com>
2022-09-20 14:42:47 -1000

    Completed Part 4 Problem Sets

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var firstString = "The background color will turn blue"
+    var secondString = "The background color will turn green"
+    var thirdString = "Hello world"
+    var fourthString = "Goodbye world"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeColorButtonPressed(_ sender: Any) {
+        if firstString == "The background color will turn red"
+        {
+            self.view.backgroundColor = UIColor.red
+            labelOneRed.text = "Red"
+        }else{
+            
+            self.view.backgroundColor = UIColor.blue
+            labelTwoBlue.text = "Blue"
+            }
+        }
+   
+    @IBOutlet var labelOneRed: UILabel!
+    
+    @IBOutlet var labelTwoBlue: UILabel!
+    
+    //Problem Set #2
+    
+    @IBAction func magicButtonPressed(_ sender: Any) {
+        if thirdString == "Hello World" {
+           
+            self.view.backgroundColor = UIColor.green
+            labelGreen.text = "Green"
+        print("I completed both problem sets") }
+        }
+    @IBOutlet var labelGreen: UILabel!
 }
-
